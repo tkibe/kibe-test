8f242d3052f1b2dfb3d58f56f46c8001ff1b16b1
t-kibe <t-kibe@mediaplex.co.jp>
2025-05-30 14:15:28 +0900

    最大公約数を計算するスクリプトとテストコードを追加

---
+++ math-gcd.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 最大公約数を計算するスクリプト
+# 使い方: ./math-gcd.sh <数値1> <数値2>
+
+# 最大公約数の計算
+math_gcd() {
+    local int_x="$1"
+    local int_y="$2"
+    local temp
+
+    # $int_xが$int_yより小さい場合、入れ替える
+    if [ "$int_x" -lt "$int_y" ]; then
+        temp="$int_x"
+        int_x="$int_y"
+        int_y="$temp"
+    fi
+
+    # $int_yが0になるまで割り算を繰り返す
+    while [ "$int_y" -ne 0 ]; do
+        temp="$int_y"
+        int_y=$((int_x % int_y))
+        int_x="$temp"
+    done
+
+    echo "$int_x"
+}
+
+# 引数の数チェック
+if [ $# -ne 2 ]; then
+    echo "引数の数が不正です。2つの数値を指定してください。"
+    exit 1
+fi
+
+# 数値チェック
+if ! [[ "$1" =~ ^[0-9]+$ ]] || ! [[ "$2" =~ ^[0-9]+$ ]]; then
+    echo "引数は正の整数である必要があります。"
+    exit 1
+fi
+
+# 0チェック
+if [ "$1" -eq 0 ] || [ "$2" -eq 0 ]; then
+    echo "0は無効な入力です。正の整数を指定してください。"
+    exit 1
+fi
+
+# 最大公約数の計算
+result=$(math_gcd "$1" "$2")
+
+# 結果の出力
+echo "${1} と ${2} の最大公約数は ${result} です"
--- test.sh
+++ test.sh
@@ -1,3 +1,54 @@
 #!/bin/bash
 
-# 最大公約数を計算するシェルスクリプト
+# GithubActionsでテスト実行するスクリプト
+# 使い方: ./test.sh
+
+test() {
+    input=("$@")
+    output=$(bash math-gcd.sh "${input[@]}" 2>/dev/null)
+
+    if [ $? -eq 0 ]; then
+        echo "正常系 - 成功: ${input[*]} => $output"
+    else
+        echo "正常系 - 失敗: ${input[*]} => '$output'"
+    fi
+}
+
+test_error() {
+    input=("$@")
+    output=$(bash math-gcd.sh "${input[@]}" 2>/dev/null)
+
+    if [ $? -ne 0 ]; then
+        echo "異常系 - 成功: ${input[*]} => $output"
+    else
+        echo "異常系 - 失敗: ${input[*]} => $output"
+    fi
+}
+
+echo "======================"
+echo "===== テスト開始 ====="
+echo "======================"
+
+echo "--- 正常系のテストを行います ---"
+
+# 正常系
+test 2 4
+test 123 45
+test 555 999
+
+echo "--- 異常系のテストを行います ---"
+
+# 異常系
+test_error 9
+test_error x 6
+test_error 5x 6
+test_error 2 y
+test_error 2 3y
+test_error x
+test_error   y
+test_error 1 y
+test_error
+
+echo "======================"
+echo "===== テスト終了 ====="
+echo "======================"
